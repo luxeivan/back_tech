48c6c6a4bf2c0091a767d6660dd39c5126bc1c84
luxeivan <luxeivan@gmail.com>
2025-08-26 11:07:59 +0300

    добавил авторизацию

--- routers/modus.js
+++ routers/modus.js
@@ -5,16 +5,38 @@ require("dotenv").config();
 const router = express.Router();
 const secretModus = process.env.SECRET_FOR_MODUS
 
+const loginStrapi = process.env.LOGIN_STRAPI
+const passwordStrapi = process.env.PASSWORD_STRAPI
+
 const urlStrapi = process.env.URL_STRAPI
 
-const jwt = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNzU1NTg3MDM4LCJleHAiOjE3NTgxNzkwMzh9.NpGFp1Vs5ylHm6DPOmKfR5x2-QXt0ccLebkYFSWJeA4"
+// const jwt = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzU2MTkyNTQxLCJleHAiOjE3NTg3ODQ1NDF9.Ouh4ddcvu4EuDe0kAgt48EMqa4SvDcCQr4klfsHztxA"
 
+async function getJwt() {
+    try {
+        const res = await axios.post(`${urlStrapi}/api/auth/local`, {
+            identifier: loginStrapi,
+            password: passwordStrapi,
+        })
+        if (res.data) {
+            console.log(res.data);
 
+            let jwt = ""
+            return res.data.jwt
+        } else {
+            return false
+        }
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
 
 router.post("/", async (req, res) => {
     const authorization = req.get("Authorization")
 
-    function sendDataSequentially(dataArray) {
+    async function sendDataSequentially(dataArray) {
+        const jwt = await getJwt()
         return dataArray.reduce(async (previousPromise, item, index) => {
             const accumulatedResults = await previousPromise;
             try {
@@ -26,7 +48,7 @@ router.post("/", async (req, res) => {
                 },
                     {
                         headers: {
-                            Authorization: jwt
+                            Authorization: `Bearer ${jwt}`
                         }
                     });
                 accumulatedResults.push({
@@ -41,9 +63,10 @@ router.post("/", async (req, res) => {
                 console.error(`Ошибка при отправке элемента ${index + 1}:`, error.message);
                 accumulatedResults.push({
                     success: false,
-                    data: item,
+                    // data: item,
                     error: error.message,
-                    index: index
+                    // id: response.data?.data.id,
+                    index: index + 1
                 });
             }
 
@@ -76,7 +99,7 @@ router.post("/", async (req, res) => {
         if (results) {
             return res.json({ status: "ok", results })
         } else {
-            return res.status(500).json({ status: "error"})
+            return res.status(500).json({ status: "error" })
         }
     } else {
         res.status(403).json({ status: "Forbidden" })
